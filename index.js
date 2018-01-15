a7a387c7e92b827a87aec074b1c414babce5ddbf
Henry Rowswell <henryrowswell@gmail.com>
2018-01-15 12:29:24 -0800

    initial commit - creates package.json and installs react react-dom and redux

---
+++ index.js
@@ -0,0 +1,9 @@
+#! /usr/bin/env node
+
+// trying to copy https://github.com/facebookincubator/create-react-app/blob/master/packages/create-react-app/index.js
+
+'use strict';
+
+// could do some version checking here (create-react-app checks node version)
+
+require('./createReactApp')
---
+++ init.js
@@ -0,0 +1,3 @@
+// export default function init() {
+//     console.log('do whatever init stuff you wanna do')
+// }
